ad8f782076c1c3bedc5a38210d5a373e2b196aae
Julian Platz <julian.platz@icloud.com>
2024-01-01 11:25:59 +0100

    changes + improvements

--- js/nav.js
+++ js/nav.js
@@ -7,24 +7,18 @@ let clicked = false;
 function toggleNavbar() {
     nav.style.height = clicked ? "0%" : "100%";
     nav.style.pointerEvents = clicked ? "none" : "auto";
-    nav.style.transition = ".5s";
 
     burgerOne.style.transform = clicked ? "rotate(0deg) translateY(0)" : "rotate(45deg) translateY(5px)";
-    burgerOne.style.transition = ".5s";
-
     burgerTwo.style.transform = clicked ? "rotate(0deg) translateY(0)" : "rotate(-45deg) translateY(-5px)";
-    burgerTwo.style.transition = ".5s";
 
     switch (nav.style.height) {
         case "100%":
-            setTimeout(function() {
+            setTimeout(() => {
                 navLinks.style.opacity = "1";
-                navLinks.style.transition = ".5s";
             }, 250);
             break;
         default:
             navLinks.style.opacity = "0";
-            navLinks.style.transition = ".25s";
             break;
     }
 
