e7a111382aae0bb7d07fcfb8cb226ab9ddbef021
JulianPlatz <julian.platz@icloud.com>
2024-01-21 13:06:48 +0100

    better readable js file

--- js/nav.js
+++ js/nav.js
@@ -1,19 +1,22 @@
-const nav = document.querySelector("nav");
+const nav       = document.querySelector("nav");
 const burgerOne = document.getElementById("burgerOne");
 const burgerTwo = document.getElementById("burgerTwo");
-const navLinks = document.getElementById("navLinks");
-let clicked = false;
+const navLinks  = document.getElementById("navLinks");
+let clicked     = false;
 
-function toggleNavbar() {
-    nav.style.height = clicked ? "0%" : "100%";
-    nav.style.pointerEvents = clicked ? "none" : "auto";
+function toggleNavbar() 
+{
+    nav.style.height            = clicked ? "0%" : "100%";
+    nav.style.pointerEvents     = clicked ? "none" : "auto";
 
-    burgerOne.style.transform = clicked ? "rotate(0deg) translateY(0)" : "rotate(45deg) translateY(5px)";
-    burgerTwo.style.transform = clicked ? "rotate(0deg) translateY(0)" : "rotate(-45deg) translateY(-5px)";
+    burgerOne.style.transform   = clicked ? "rotate(0deg) translateY(0)" : "rotate(45deg) translateY(5px)";
+    burgerTwo.style.transform   = clicked ? "rotate(0deg) translateY(0)" : "rotate(-45deg) translateY(-5px)";
 
-    switch (nav.style.height) {
+    switch (nav.style.height) 
+    {
         case "100%":
-            setTimeout(() => {
+            setTimeout(() => 
+            {
                 navLinks.style.opacity = "1";
             }, 250);
             break;
