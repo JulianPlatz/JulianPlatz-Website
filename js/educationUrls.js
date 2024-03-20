9e2a944d617052519ddda0b44de02ad43434b0cc
JulianPlatz <julian.platz@icloud.com>
2024-03-20 19:21:25 +0100

    better education urls

---
+++ js/educationUrls.js
@@ -0,0 +1,11 @@
+function proofitUrl() {
+    window.open('https://www.oszimt.de', '_blank');
+}
+
+function oszimtUrl() {
+    window.open('https://www.oszimt.de', '_blank');
+}
+
+function schuleanderdahmeUrl() {
+    window.open('https://www.oszimt.de', '_blank');
+}
